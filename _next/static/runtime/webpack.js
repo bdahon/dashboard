93d614d06ea4c5ff8b07769649ed776a6239056e
Binnette <binnette@gmail.com>
2020-03-22 13:08:12 +0100

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "18980e6cb5318d1d8413";
+/******/ 	var hotCurrentHash = "a7037b28b086a5257eb8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
